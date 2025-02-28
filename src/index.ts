6358381d57432d47388cc6cb171b6f5c333043f3
Michael Zanggl <michaelzanggl@gmail.com>
2025-02-28 21:40:12 +0900

    Remove callback option

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,11 @@ import Mappable from "./objects/Mappable"
 import { MapValue } from './types'
 import { times } from './array'
 
-type Callback<T> = (result: T) => any
-
 /**
  * Create a flooent string. You have access to [everything from the native String object](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String).
  */
-function string(value: string): Stringable
-function string(value: string, callback: Callback<Stringable>): string
-function string(value: string, callback?: Callback<Stringable>) {
-    const result = new Stringable(value)
-    if (!callback) return result
-    const callbackResult = callback(result)
-    return callbackResult instanceof Stringable ? callbackResult.valueOf() : callbackResult
+function string(value: string) {
+    return new Stringable(value)
 }
 string.macro = (key: string, callback: Function) => (Stringable.prototype[key] = callback)
 
--- test/given.spec.js
+++ test/given.spec.js
@@ -26,8 +26,3 @@ test('can extend flooent objects through macros', assert => {
   assert.equal(given.string('hello').scream(), 'HELLO')
   assert.equal(given.array([1]).stringify(), '1')
 })
-
-test('turns flooent variant back into raw value', assert => {
-  assert.typeOf(given.string('hello', str => '!'), 'string')
-  assert.typeOf(given.string('hello', str => 1), 'number')
-})
